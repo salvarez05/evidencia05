60e71ac7993d4e0166446cdc635316d2f0cab3d0
Samuel_Alvarez <s.alvarez05@ufromail.cl>
2023-04-24 15:13:57 -0400

    Creación basica de clase de Perro y subida de diagrama de comparacion entre POO y vida real de un perro

--- src/src/main/java/Perro.java
+++ src/src/main/java/Perro.java
@@ -1,3 +1,22 @@
 public class Perro {
+    String nombre;
+    String raza;
+    int edad;
+    String colordepelo;
 
+
+
+
+
+    public void comer(){
+    }
+    public void dormir(){
+    }
+
+    public void  ladrar(){
+
+    }
+
+    public void moverLaCola(){}
+    public void correr(){}
 }
